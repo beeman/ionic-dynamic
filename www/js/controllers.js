92f8e7c84234b75ba417b9f30dce35ba8fe4c63a
Bram Borggreve <borggreve@gmail.com>
2015-03-06 01:51:30 +0100

    Upgrade ionic to beta 14

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,128 @@
 var app = angular.module('starter.controllers', []);
 
+/**
+ * This variable will store the base configuration which we get using the config service in AppCtrl
+ */
+app.baseConfig = false;
+
+/**
+ * This controllers reads the configuration and adds the states
+ */
+app.controller('InitCtrl', function ($scope, $state, $timeout, $ionicHistory, config) {
+    $scope.startLoading();
+
+    $ionicHistory.nextViewOptions({
+        disableAnimate: true,
+        disableBack: true,
+        historyRoot: true
+    });
+
+    var existingStates = [];
+
+    var addStates = function (states) {
+        existingStates = getExistingStates();
+        for (var idx in states) {
+            addState(states[idx]);
+        }
+    };
+
+    var getExistingStates = function () {
+        var states = $state.get();
+        var result = [];
+        for (var idx in states) {
+            result.push(states[idx].name)
+        }
+        return result;
+    };
+
+    var stateExist = function (state) {
+        return existingStates.indexOf(state) !== -1;
+    };
+
+    var setDefaultState = function (state) {
+        // Store defaultState in defaultStateSafe as it gets overwritten while re-initializing
+        app.baseConfig.defaultState = app.defaultStateSafe = state.state;
+        app.urlRouterProvider.otherwise(state.state);
+    };
+
+    var addState = function (state) {
+        app.baseConfig.defaultState = app.defaultStateSafe;
+
+        // Only add states that are not there yet
+        if (!stateExist(state.state)) {
+            if (state.defaultState) {
+                setDefaultState(state);
+            }
+            app.stateProvider.state(state.state, {
+                url: state.url,
+                views: {
+                    'menuContent': {
+                        templateUrl: state.templateUrl,
+                        controller: state.controller
+                    }
+                }
+            });
+        }
+    };
+
+    $timeout(function () {
+
+        // Read the configuration
+        config.config().then(function (response) {
+
+            // Set the baseConfig
+            app.baseConfig = response.data;
+
+            // Add the states
+            addStates(app.baseConfig.states);
+
+            // Go to the default state
+            $state.go(app.baseConfig.defaultState, {}, {location: true})
+        });
+
+    }, 1000);
+
+});
+
+/**
+ * The AppCtrl takes care of the parent view for all the other views
+ * It is defined in one of the two static states in this application
+ */
+app.controller('AppCtrl', function ($scope, $state, $ionicHistory) {
+    $scope.stopLoading();
+
+    $ionicHistory.nextViewOptions({
+        disableAnimate: true,
+        disableBack: true,
+        historyRoot: true
+    });
+
+    $scope.appName = app.baseConfig.appName;
+    $scope.menuItems = app.baseConfig.menuItems;
+
+});
+
+/**
+ * The LayoutCtrl is attached to the body
+ */
+app.controller('LayoutCtrl', function ($scope, $state, $ionicLoading) {
+    $scope.init = function () {
+        $state.go('init', true);
+    };
+    $scope.defaultState = function () {
+        $state.go(app.baseConfig.defaultState, true);
+    };
+    $scope.startLoading = function () {
+        $ionicLoading.show({
+            template: 'Loading...'
+        });
+    };
+    $scope.stopLoading = function () {
+        $ionicLoading.hide();
+    };
+});
+
+
 app.controller('DashboardCtrl', function ($scope, $state) {
     $scope.pageTitle = 'Dashboard';
 });
@@ -15,5 +138,3 @@ app.controller('SandboxCtrl', function ($scope, $state) {
     $scope.pageTitle = 'Sandbox';
     $scope.pageContent = 'Sandbox page';
 });
-
-
